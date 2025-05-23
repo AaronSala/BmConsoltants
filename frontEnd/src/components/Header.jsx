df053d49d3a34c07ce403e3bfb8571d9c3f1a805
Aaron Sala <salaaron2@gmail.com>
2025-05-23 16:03:22 +0300

    initialized the setup for the websiteboth front end and backend

---
+++ frontEnd/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const Header = () => {
+  return (
+    <>
+      <div className="header w-100 ps-4 pt-1 border-bottom-3">
+        <img src="bmlogo.jpg" className="logo w-10 h-20 img-fluid " />
+      </div>
+    </>
+  );
+};
+
+export default Header;
