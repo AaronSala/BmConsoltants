f8b1ba1e56b994110ef520195550e9ef3d56c49c
Aaron Sala <salaaron2@gmail.com>
2025-05-24 02:20:06 +0300

    designed the header and the hero sections

--- frontEnd/src/components/Header.jsx
+++ frontEnd/src/components/Header.jsx
@@ -1,13 +1,52 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/header.css";
+
+import Hero from "./Hero";
 
 const Header = () => {
   return (
-    <>
-      <div className="header w-100 ps-4 pt-1 border-bottom-3">
-        <img src="bmlogo.jpg" className="logo w-10 h-20 img-fluid " />
-      </div>
-    </>
+    <div id="header-hero-container">
+      <header>
+        <div className="flex container">
+          <a href="#" id="logo" className="">
+            <img src="/Pasted image.png" />
+          </a>
+          <nav>
+            <button id="nav-toggle" className="humburger-menu">
+              <span className="strip"></span>
+              <span className="strip"></span>
+              <span className="strip"></span>
+            </button>
+
+            <ul id="nav-menu">
+              <li>
+                <a href="#" className="active">
+                  Home
+                </a>
+              </li>
+              <li>
+                <a href="#">Products</a>
+              </li>
+             
+              <li>
+                <a href="#">Contacts</a>
+              </li>
+              <li>
+                <a href="#">About</a>
+              </li>
+              <li>
+                <a href="#">News</a>
+              </li>
+              <li id="close-flyout">
+                <span className="fas fa-times"></span>
+              </li>
+            </ul>
+          </nav>
+        </div>
+      </header>
+      <Hero />
+    </div>
   );
 };
 
--- frontEnd/src/components/Hero.jsx
+++ frontEnd/src/components/Hero.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import "bootstrap/dist/css/bootstrap.min.css";
+import './styles/hero.css'
 
 const Hero = () => {
   return (
     <div>
-      
+      <section id="hero">
+        <div className="fade ">
+          <div className="hero-text ">
+           
+            <h1>For Quality Images And Videos </h1>
+            <p>
+              Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+              Laboriosam recusandae
+              <br />
+              ipsa illum animi unde vitae amet rem, maiores accusantium
+              incidunt?
+            </p>
+          </div>
+        </div>
+      </section>
     </div>
-  )
+  );
 }
 
 export default Hero
