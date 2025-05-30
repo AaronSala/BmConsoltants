2d4386cb77f12e60c666c9c7a35d8f0b2de8ae50
Aaron Sala <salaaron2@gmail.com>
2025-05-30 03:01:53 +0300

    created the basic components of the testimonials class

--- frontEnd/src/components/Testimonials.jsx
+++ frontEnd/src/components/Testimonials.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
-import "./styles/testimonials.css"
+import React from "react";
+import "./styles/testimonials.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { PrevArrow, NextArrow } from "@fortawesome/free-solid-svg-icons";
 
 const Testimonials = () => {
   return (
-      <div className='testimonies'>
-          <h2>What Our Customers say</h2>
-          <div className='quotes'>
-              <p>"The best I have ever fount No delays and The Best capture of all time"</p>
-              <span></span>
-          </div>
-      
+    <div className="testimonies">
+      <FontAwesomeIcon icon={PrevArrow} />
+      <h2>What Our Customers say</h2>
+      <div className="quotes">
+        <blockquote>
+          "The best I have ever fount No delays and The Best capture of all time
+          <br />
+          They promise they deliver in time "
+        </blockquote>
+        <span></span>
+      </div>
+      <FontAwesomeIcon icon={NextArrow} />
     </div>
-  )
-}
+  );
+};
 
-export default Testimonials
+export default Testimonials;
