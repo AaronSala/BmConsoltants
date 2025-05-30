3542a97cf5ea0fe9bc9f3d7600c1642626e196e0
Aaron Sala <salaaron2@gmail.com>
2025-05-30 15:51:21 +0300

    done with the comments section and thee styling

--- frontEnd/src/components/Slider.jsx
+++ frontEnd/src/components/Slider.jsx
@@ -3,9 +3,8 @@ import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import "./styles/slider.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { generateStars } from "./Stars"; 
+
+import { generateStars } from "./Stars";
 
 const Sliders = () => {
   const stars = generateStars(600); // number of stars
--- frontEnd/src/components/Testimonials.jsx
+++ frontEnd/src/components/Testimonials.jsx
@@ -1,22 +1,65 @@
 import React from "react";
+import Slider from "react-slick";
 import "./styles/testimonials.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { PrevArrow, NextArrow } from "@fortawesome/free-solid-svg-icons";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
+// Custom arrow components
+const PrevArrow = ({ className, onClick }) => (
+  <div className={className} onClick={onClick}>
+    &#60;
+  </div>
+);
+
+const NextArrow = ({ className, onClick }) => (
+  <div className={className} onClick={onClick}>
+    &#62;
+  </div>
+);
 
 const Testimonials = () => {
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: true,
+    prevArrow: <PrevArrow />,
+    nextArrow: <NextArrow />,
+  };
+
   return (
-    <div className="testimonies">
-      <FontAwesomeIcon icon={PrevArrow} />
-      <h2>What Our Customers say</h2>
-      <div className="quotes">
-        <blockquote>
-          "The best I have ever fount No delays and The Best capture of all time
-          <br />
-          They promise they deliver in time "
-        </blockquote>
-        <span></span>
-      </div>
-      <FontAwesomeIcon icon={NextArrow} />
+    <div className="testimonies container">
+      <h2 className="pb-4">What Our Customers say</h2>
+      <Slider {...settings}>
+        <div className="quotes ">
+          <div className="image">
+            <img src="#" alt="user Image" />
+          </div>
+          <span>User name1</span>
+
+          <blockquote>
+            "The best I have ever found. No delays and the best capture of all
+            time.
+            <br />
+            They promise and they deliver in time."
+          </blockquote>
+        </div>
+        <div className="quotes">
+          <div className="image">
+            <img src="#" alt="user Image" />
+          </div>
+          <span>User name2</span>
+          <blockquote>
+            "The best I have ever found. No delays and the best capture of all
+            time.
+            <br />
+            They promise and they deliver in time."
+          </blockquote>
+        </div>
+        {/* Add more slides here if needed */}
+      </Slider>
     </div>
   );
 };
