d69c1079b3502a987fb07ded5536b5836e7d03f3
Aaron Sala <salaaron2@gmail.com>
2025-07-18 11:35:29 +0300

    run build

--- frontEnd/vite.config.js
+++ frontEnd/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
+import path from "path";
+
+export default defineConfig(({ mode }) => ({
+  base: mode === "production" ? "/BmConsoltants/" : "/",
+  server: {
+    host: "::",
+    port: 5173, // or any port you prefer
+  },
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+}));
